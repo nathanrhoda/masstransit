a67508951ecdcd958d2190794b2b4abde07b983f
Nathan Rhoda <nathan.rhoda@hotmail.com>
2021-04-03 22:01:04 +0200

    Barebones masstransit configured with a seperate consumer

---
+++ src/Bank/AccountConsumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using Bank.Messages;
+using MassTransit;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AccountConsumer
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+               
+
+                cfg.ReceiveEndpoint("Message", e =>
+                {
+                    e.Consumer<EventConsumer>();
+                });
+            });
+
+
+            var source = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+
+            await busControl.StartAsync(source.Token);
+            try
+            {
+                Console.WriteLine("Press enter to exit");
+
+                await Task.Run(() => Console.ReadLine());
+            }
+            finally
+            {
+                await busControl.StopAsync();
+            }
+        }
+
+        class EventConsumer :
+        IConsumer<Message>
+        {
+            public async Task Consume(ConsumeContext<Message> context)
+            {
+                Console.WriteLine("Value: {0}", context.Message.Text);
+            }
+        }
+
+       
+    }
+}
---
+++ src/Bank/Bank.Api/Controllers/AtmController.cs
@@ -0,0 +1,29 @@
+﻿using Bank.Messages;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Bank.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AtmController : ControllerBase
+    {
+        private IBus _bus { get; set; }
+
+        public AtmController(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        [HttpPost("Balance")]
+        public async Task<ActionResult<string>> Balance(string accountNumber)
+        {
+            string messageText = $"Account Number {accountNumber} The time is {DateTimeOffset.Now}";
+            await _bus.Publish(new Message { Text = messageText });
+            Console.WriteLine(messageText);
+            return Ok($"Message Sent Account Number: {accountNumber}");
+        }
+    }
+}
